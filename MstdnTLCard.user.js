4cf4cc67d04462a6ac186e9ded3423df6b3825d5
hayabusa <hs-sh-net@users.noreply.github.com>
2018-03-05 19:06:27 +0900

    Youtube・Soundcloud等を埋め込めるように

--- MstdnTLCard.user.js
+++ MstdnTLCard.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         mastodonのTLにPreviewCardを表示するやつ
-// @namespace    https://github.com/theoria24/MstdnTimelinePreviewCard
-// @version      0.1
+// @namespace    https://github.com/hs-sh-net/MstdnTimelinePreviewCard
+// @version      0.1.1
 // @description  PreviewCardをTLに表示します
-// @author       theoria
-// @match        https://theboss.tech/web/*
+// @author       theoria, hs-sh.net
+// @match        https://otoya.space/web/*
 // @grant        none
 // @require      http://code.jquery.com/jquery-2.2.4.min.js
 // @license      MIT License
@@ -12,14 +12,19 @@
 
 var INSTANCE = $(location).attr('host');
 
-function card_formater(url, title, type, description, image, width, height) {
+function card_formater(url, title, type, description, content, width, height) {
   if (type == "photo") {
-    return '<a href="' + url + '" class="status-card horizontal" target="_blank" rel="noopener"><div class="status-card__image"><img src="' + image + '" alt="' + title + '" class="status-card__image-image" width="' + width + '" height="' + height + '"></div><div class="status-card__content"><strong class="status-card__title" title="' + title + '">' + title + '</strong><span class="status-card__host">' + url.match(/^https?:\/\/(.*?)\//)[1] + '</span></div></a>';
-  } else if (width > height) {
-    return '<a href="' + url + '" class="status-card horizontal" target="_blank" rel="noopener"><div class="status-card__image"><img src="' + image + '" alt="' + title + '" class="status-card__image-image" width="' + width + '" height="' + height + '"></div><div class="status-card__content"><strong class="status-card__title" title="' + title + '">' + title + '</strong><span class="status-card__host">' + url.match(/^https?:\/\/(.*?)\//)[1] + '</span></div></a>';
-  } else {
-    return '<a href="' + url + '" class="status-card" target="_blank" rel="noopener"><div class="status-card__image"><img src="' + image + '" alt="' + title + '" class="status-card__image-image" width="' + width + '" height="' + height + '"></div><div class="status-card__content"><strong class="status-card__title" title="' + title + '">' + title + '</strong><p class="status-card__description">' + description + '</p><span class="status-card__host">' + url.match(/^https?:\/\/(.*?)\//)[1] + '</span></div></a>';
+    return '<a href="' + url + '" class="status-card horizontal" target="_blank" rel="noopener"><div class="status-card__image"><img src="' + content + '" alt="' + title + '" class="status-card__image-image" width="' + width + '" height="' + height + '"></div><div class="status-card__content"><strong class="status-card__title" title="' + title + '">' + title + '</strong><span class="status-card__host">' + url.match(/^https?:\/\/(.*?)\//)[1] + '</span></div></a>';
+  } else if(type == "link"){
+    if (width > height) {
+      return '<a href="' + url + '" class="status-card horizontal" target="_blank" rel="noopener"><div class="status-card__image"><img src="' + content + '" alt="' + title + '" class="status-card__image-image" width="' + width + '" height="' + height + '"></div><div class="status-card__content"><strong class="status-card__title" title="' + title + '">' + title + '</strong><span class="status-card__host">' + url.match(/^https?:\/\/(.*?)\//)[1] + '</span></div></a>';
+    } else {
+      return '<a href="' + url + '" class="status-card" target="_blank" rel="noopener"><div class="status-card__image"><img src="' + content + '" alt="' + title + '" class="status-card__image-image" width="' + width + '" height="' + height + '"></div><div class="status-card__content"><strong class="status-card__title" title="' + title + '">' + title + '</strong><p class="status-card__description">' + description + '</p><span class="status-card__host">' + url.match(/^https?:\/\/(.*?)\//)[1] + '</span></div></a>';
+    }
+  } else if (type == "video"){
+      return '<div class="status-card-video">' + content + '</div>';
   }
+    return '';
 }
 
 (function() {
@@ -29,9 +34,11 @@ function card_formater(url, title, type, description, image, width, height) {
       $(this).addClass("carded");
       var id = $(this);
       $.getJSON("https://" + INSTANCE + "/api/v1/statuses/" + id.attr('data-id') + "/card").done(function(data) {
-        if (data.image) {
-          $(id).find('.status__content').after(card_formater(data.url, data.title, data.description, data.image, data.width, data.height));
-        }
+        if (data.image != null && data.image != "") {
+          $(id).find('.status__content').after(card_formater(data.url, data.title, data.type, data.description, data.image, data.width, data.height));
+        } else if (data.html != null && data.html != ""){
+          $(id).find('.status__content').after(card_formater(data.url, data.title, data.type, data.description, data.html, data.width, data.height));
+        } // else {$(id).find('.status__content').after("None");}
       });
     });
     (new MutationObserver(function(MutationRecords, MutationObserver) {
@@ -39,13 +46,15 @@ function card_formater(url, title, type, description, image, width, height) {
         $(this).addClass("carded");
         var id = $(this);
         $.getJSON("https://" + INSTANCE + "/api/v1/statuses/" + id.attr('data-id') + "/card").done(function(data) {
-          if (data.image) {
+          if (data.image != null && data.image != "") {
             $(id).find('.status__content').after(card_formater(data.url, data.title, data.type, data.description, data.image, data.width, data.height));
-          }
+          } else if (data.html != null && data.html != ""){
+            $(id).find('.status__content').after(card_formater(data.url, data.title, data.type, data.description, data.html, data.width, data.height));
+          } // else {$(id).find('.status__content').after("None");}
         });
       });
     })).observe($('.item-list').get(0), {
       childList: true,
     });
-  }, 10000);
+  }, 5000);
 })();
