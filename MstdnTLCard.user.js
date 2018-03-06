ef6bf8e21e401468cca57ef66891bbbb3c8b0500
theoria <theoria23@gmail.com>
2018-03-06 20:47:04 +0900

    コード整形

--- MstdnTLCard.user.js
+++ MstdnTLCard.user.js
@@ -14,20 +14,20 @@ var INSTANCE = $(location).attr('host');
 
 function card_formater(url, title, type, description, content, width, height) {
   if (description.length > 50) {
-    description = description.substr(0,50);
+    description = description.substr(0, 50);
   }
   if (type == "photo") {
     return '<a href="' + url + '" class="status-card horizontal" target="_blank" rel="noopener"><div class="status-card__image"><img src="' + content + '" alt="' + title + '" class="status-card__image-image" width="' + width + '" height="' + height + '"></div><div class="status-card__content"><strong class="status-card__title" title="' + title + '">' + title + '</strong><span class="status-card__host">' + url.match(/^https?:\/\/(.*?)\//)[1] + '</span></div></a>';
-  } else if(type == "link"){
+  } else if (type == "link") {
     if (width > height) {
       return '<a href="' + url + '" class="status-card horizontal" target="_blank" rel="noopener"><div class="status-card__image"><img src="' + content + '" alt="' + title + '" class="status-card__image-image" width="' + width + '" height="' + height + '"></div><div class="status-card__content"><strong class="status-card__title" title="' + title + '">' + title + '</strong><span class="status-card__host">' + url.match(/^https?:\/\/(.*?)\//)[1] + '</span></div></a>';
     } else {
       return '<a href="' + url + '" class="status-card" target="_blank" rel="noopener"><div class="status-card__image"><img src="' + content + '" alt="' + title + '" class="status-card__image-image" width="' + width + '" height="' + height + '"></div><div class="status-card__content"><strong class="status-card__title" title="' + title + '">' + title + '</strong><p class="status-card__description">' + description + '</p><span class="status-card__host">' + url.match(/^https?:\/\/(.*?)\//)[1] + '</span></div></a>';
     }
-  } else if (type == "video"){
-      return '<div class="status-card-video">' + content + '</div>';
+  } else if (type == "video") {
+    return '<div class="status-card-video">' + content + '</div>';
   }
-    return '';
+  return '';
 }
 
 (function() {
@@ -39,7 +39,7 @@ function card_formater(url, title, type, description, content, width, height) {
       $.getJSON("https://" + INSTANCE + "/api/v1/statuses/" + id.attr('data-id') + "/card").done(function(data) {
         if (data.image != null && data.image != "") {
           $(id).find('.status__content').after(card_formater(data.url, data.title, data.type, data.description, data.image, data.width, data.height));
-        } else if (data.html != null && data.html != ""){
+        } else if (data.html != null && data.html != "") {
           $(id).find('.status__content').after(card_formater(data.url, data.title, data.type, data.description, data.html, data.width, data.height));
         } // else {$(id).find('.status__content').after("None");}
       });
@@ -51,7 +51,7 @@ function card_formater(url, title, type, description, content, width, height) {
         $.getJSON("https://" + INSTANCE + "/api/v1/statuses/" + id.attr('data-id') + "/card").done(function(data) {
           if (data.image != null && data.image != "") {
             $(id).find('.status__content').after(card_formater(data.url, data.title, data.type, data.description, data.image, data.width, data.height));
-          } else if (data.html != null && data.html != ""){
+          } else if (data.html != null && data.html != "") {
             $(id).find('.status__content').after(card_formater(data.url, data.title, data.type, data.description, data.html, data.width, data.height));
           } // else {$(id).find('.status__content').after("None");}
         });
