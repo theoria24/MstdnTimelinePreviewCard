103f4014bd060204ab64b0e3d18e8bcb9bdad28d
eai04191 <eai@mizle.net>
2018-08-24 17:59:53 +0900

    Lint

--- MstdnTLCard.user.js
+++ MstdnTLCard.user.js
@@ -10,55 +10,152 @@
 // @license      MIT License
 // ==/UserScript==
 
-var INSTANCE = $(location).attr('host');
+var INSTANCE = $(location).attr("host");
 
 function card_formater(url, title, type, description, content, width, height) {
   if (description.length > 50) {
     description = description.substr(0, 50);
   }
   if (type == "photo") {
-    return '<a href="' + url + '" class="status-card horizontal" target="_blank" rel="noopener"><div class="status-card__image"><img src="' + content + '" alt="' + title + '" class="status-card__image-image" width="' + width + '" height="' + height + '"></div><div class="status-card__content"><strong class="status-card__title" title="' + title + '">' + title + '</strong><span class="status-card__host">' + url.match(/^https?:\/\/(.*?)\//)[1] + '</span></div></a>';
+    return `
+            <a href="${url}" class="status-card horizontal" target="_blank" rel="noopener">
+                <div class="status-card__image">
+                    <img src="${content}" alt="${title}" class="status-card__image-image" width="${width}" height="${height}">
+                </div>
+                <div class="status-card__content">
+                    <strong class="status-card__title" title="${title}">${title}</strong>
+                    <span class="status-card__host">${
+                      url.match(/^https?:\/\/(.*?)\//)[1]
+                    }</span>
+                </div>
+            </a>`;
   } else if (type == "link") {
     if (width > height) {
-      return '<a href="' + url + '" class="status-card horizontal" target="_blank" rel="noopener"><div class="status-card__image"><img src="' + content + '" alt="' + title + '" class="status-card__image-image" width="' + width + '" height="' + height + '"></div><div class="status-card__content"><strong class="status-card__title" title="' + title + '">' + title + '</strong><span class="status-card__host">' + url.match(/^https?:\/\/(.*?)\//)[1] + '</span></div></a>';
+      return `
+                <a href="${url}" class="status-card horizontal" target="_blank" rel="noopener">
+                    <div class="status-card__image">
+                        <img src="${content}" alt="${title}" class="status-card__image-image" width="${width}" height="${height}">
+                    </div>
+                    <div class="status-card__content">
+                        <strong class="status-card__title" title="${title}">${title}</strong>
+                        <span class="status-card__host">${
+                          url.match(/^https?:\/\/(.*?)\//)[1]
+                        }</span>
+                    </div>
+                </a>'`;
     } else {
-      return '<a href="' + url + '" class="status-card" target="_blank" rel="noopener"><div class="status-card__image"><img src="' + content + '" alt="' + title + '" class="status-card__image-image" width="' + width + '" height="' + height + '"></div><div class="status-card__content"><strong class="status-card__title" title="' + title + '">' + title + '</strong><p class="status-card__description">' + description + '</p><span class="status-card__host">' + url.match(/^https?:\/\/(.*?)\//)[1] + '</span></div></a>';
+      return `
+            <a href="${url}" class="status-card" target="_blank" rel="noopener">
+                <div class="status-card__image">
+                    <img src="${content}" alt="${title}" class="status-card__image-image" width="${width}" height="${height}">
+                </div>
+                <div class="status-card__content"><strong class="status-card__title" title="${title}">${title}</strong>
+                    <p class="status-card__description">${description}</p>
+                    <span class="status-card__host">${
+                      url.match(/^https?:\/\/(.*?)\//)[1]
+                    }</span>
+                </div>
+            </a>`;
     }
   } else if (type == "video") {
-    return '<div class="status-card-video">' + content + '</div>';
+    return `<div class="status-card-video">${content}</div>`;
   }
-  return '';
+  return "";
 }
 
 (function() {
-  'use strict';
+  "use strict";
   setTimeout(function() {
-    $('article:not(:has(.notification)) > div > .status__wrapper > .status:not(.carded)').each(function() {
+    $(
+      "article:not(:has(.notification)) > div > .status__wrapper > .status:not(.carded)"
+    ).each(function() {
       $(this).addClass("carded");
       var id = $(this);
-      $.getJSON("https://" + INSTANCE + "/api/v1/statuses/" + id.attr('data-id') + "/card").done(function(data) {
+      $.getJSON(
+        "https://" +
+          INSTANCE +
+          "/api/v1/statuses/" +
+          id.attr("data-id") +
+          "/card"
+      ).done(function(data) {
         if (data.html != null && data.html != "") {
-          $(id).find('.status__content').after(card_formater(data.url, data.title, data.type, data.description, data.html, data.width, data.height));
+          $(id)
+            .find(".status__content")
+            .after(
+              card_formater(
+                data.url,
+                data.title,
+                data.type,
+                data.description,
+                data.html,
+                data.width,
+                data.height
+              )
+            );
         } else if (data.image != null && data.image != "") {
-          $(id).find('.status__content').after(card_formater(data.url, data.title, data.type, data.description, data.image, data.width, data.height));
+          $(id)
+            .find(".status__content")
+            .after(
+              card_formater(
+                data.url,
+                data.title,
+                data.type,
+                data.description,
+                data.image,
+                data.width,
+                data.height
+              )
+            );
         } // else {$(id).find('.status__content').after("None");}
       });
     });
-    (new MutationObserver(function(MutationRecords, MutationObserver) {
-      $('article:not(:has(.notification)) > div > .status__wrapper > .status:not(.carded)').each(function() {
+    new MutationObserver(function(MutationRecords, MutationObserver) {
+      $(
+        "article:not(:has(.notification)) > div > .status__wrapper > .status:not(.carded)"
+      ).each(function() {
         $(this).addClass("carded");
         var id = $(this);
-        $.getJSON("https://" + INSTANCE + "/api/v1/statuses/" + id.attr('data-id') + "/card").done(function(data) {
+        $.getJSON(
+          "https://" +
+            INSTANCE +
+            "/api/v1/statuses/" +
+            id.attr("data-id") +
+            "/card"
+        ).done(function(data) {
           if (data.html != null && data.html != "") {
-            $(id).find('.status__content').after(card_formater(data.url, data.title, data.type, data.description, data.html, data.width, data.height));
+            $(id)
+              .find(".status__content")
+              .after(
+                card_formater(
+                  data.url,
+                  data.title,
+                  data.type,
+                  data.description,
+                  data.html,
+                  data.width,
+                  data.height
+                )
+              );
           } else if (data.image != null && data.image != "") {
-            $(id).find('.status__content').after(card_formater(data.url, data.title, data.type, data.description, data.image, data.width, data.height));
+            $(id)
+              .find(".status__content")
+              .after(
+                card_formater(
+                  data.url,
+                  data.title,
+                  data.type,
+                  data.description,
+                  data.image,
+                  data.width,
+                  data.height
+                )
+              );
           } // else {$(id).find('.status__content').after("None");}
         });
       });
-    })).observe($('.columns-area').get(0), {
+    }).observe($(".columns-area").get(0), {
       childList: true,
-      subtree: true,
+      subtree: true
     });
   }, 5000);
 })();
